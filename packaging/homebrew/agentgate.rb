ddb5e0eb4b32c23bbcb517e9c75513a680558be0
麻凡 <my@dengmengmian.com>
2026-09-16 23:57:55 +0800

    Keep Homebrew cask sha256 in sync with v2.0.7

--- packaging/homebrew/agentgate.rb
+++ packaging/homebrew/agentgate.rb
@@ -3,8 +3,8 @@ cask "agentgate" do
   arch arm: "aarch64", intel: "x64"
 
   version "2.0.7"
-  sha256 arm:   "7bdd08ee99a29470f2ba05a1d3ded2a187f3d578419fccd75c715ee8160812bc",
-         intel: "07fb46c6f53c32b68c3083b4540733cefbc50c2464a79beb78bed9124250292e"
+  sha256 arm:   "ffe62dbb26fb0909b19ce047e5e2f57209a970d6ee97ee70b384b9056dba01a2",
+         intel: "6788cb57fa2e96134904b26eff5d2ac6af2c871804444d180a47fa5e55be82de"
 
   url "https://github.com/dengmengmian/muxlayer/releases/download/v#{version}/MuxLayer_#{version}_#{arch}.dmg",
       verified: "github.com/dengmengmian/muxlayer/"
